7eccd12564364901e7f92ff0a187fb3a8c30ab9c
Pixaler <gnidko02@gmail.com>
2023-05-18 20:15:20 +0200

    Update CodingSetup script

--- windows/coding_setup/_CodingSetup.ps1
+++ windows/coding_setup/_CodingSetup.ps1
@@ -3,16 +3,17 @@ irm get.scoop.sh -outfile '.\install.ps1'
 rm .\install.ps1
 scoop bucket add main
 scoop bucket add extras	
+scoop bucket add versions
 scoop install pwsh oh-my-posh
 scoop install windows-terminal
 mkdir "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal"
 cp ".\settings.json" "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal"
 cp -r ".\Powershell" "C:\!MYCOR"
-scoop install python nodejs
+scoop install python310 main/nodejs-lts
 w10uninstall.bat
 $input = Read-Host -Prompt "Press any button to continue"
 cp ".\winterm.bat" "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 scoop install zig
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim C:/Users/$env:UserName/AppData/Local/nvim
-git clone https://github.com/Pixaler/astronvim_config C:/Users/$env:UserName/AppData/Local/nvim/lua/user
+# git clone --depth 1 https://github.com/AstroNvim/AstroNvim C:/Users/$env:UserName/AppData/Local/nvim
+# git clone https://github.com/Pixaler/astronvim_config C:/Users/$env:UserName/AppData/Local/nvim/lua/user
 scoop install lazygit
