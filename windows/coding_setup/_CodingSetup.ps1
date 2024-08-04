bdc64a8c8ba2eec06c72092bf26178939cccfa88
Pixaler <gnidko02@gmail.com>
2024-08-04 07:23:35 +0200

    Neovim and windowsterminal fix

--- windows/coding_setup/_CodingSetup.ps1
+++ windows/coding_setup/_CodingSetup.ps1
@@ -6,14 +6,14 @@ scoop bucket add extras
 scoop bucket add versions
 scoop install pwsh oh-my-posh
 scoop install windows-terminal
-mkdir "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal"
-cp ".\settings.json" "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal"
+mkdir "C:\Users\$env:UserName\scoop\apps\windows-terminal\current\settings"
+cp ".\settings.json" "C:\Users\$env:UserName\scoop\apps\windows-terminal\current\settings"
 cp -r ".\Powershell" "C:\!MYCOR"
 scoop install python310 main/nodejs-lts
 w10uninstall.bat
 $input = Read-Host -Prompt "Press any button to continue"
 cp ".\winterm.bat" "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 scoop install zig
-# git clone --depth 1 https://github.com/AstroNvim/AstroNvim C:/Users/$env:UserName/AppData/Local/nvim
-# git clone https://github.com/Pixaler/astronvim_config C:/Users/$env:UserName/AppData/Local/nvim/lua/user
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+cp -r "D:\07_Projects\my_configs\linux\.config\nvim" "$env:LOCALAPPDATA"
 scoop install lazygit
